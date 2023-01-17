76ff73c0c4f81b652f2d0e80321a2db0acc2a8e5
Amir.sh <mobilefactory012@gmail.com>
2023-01-17 20:38:32 +0330

    Add :Vazir font &  remove extra file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import SmartWAtch from "./components/SmartWatch"
 
 function App() {
   return (
-    <div dir="rtl" className="App bg-slate-300">
+    <div className="App bg-slate-300">
       <SmartWAtch />
     </div>
   )
